4bf3bc455edd5a6af76bd68e7d246d2da6407b95
John Memmott <jmemmott@Johns-MacBook-Pro.local>
2023-05-23 19:21:52 -0600

    rewrite: install core; use homebrew only

--- install.sh
+++ install.sh
@@ -1,53 +1,74 @@
 #!/bin/bash
 
-# Set XDG_CONFIG_HOME and ZDOTDIR variables in .zshenv
-touch ~/.zshenv
-echo 'export XDG_CONFIG_HOME="~"' >>~/.zshenv
-echo 'export ZDOTDIR="$XDG_CONFIG_HOME"' >>~/.zshenv
+# Function to install brew on macOS or Linux
+install_brew() {
+    if ! command -v brew &>/dev/null; then
+        echo "Installing brew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+        echo "Brew is already installed."
+    fi
+}
 
-if [[ "$(uname)" == "Darwin" ]]; then
-  # Check if Homebrew is installed
-  if ! command -v brew &>/dev/null; then
-    # Install Homebrew
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  else
-    # Update Homebrew
-    brew update
-  fi
-
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-  # Add Homebrew shell environment to .zshrc
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>"~/.zshrc"
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-
-  # Install ansible
-  brew install ansible
-
-elif [[ "$(uname)" == "Linux" ]]; then
-  # Update package manager
-  apt update
-
-  # Install git
-  apt install -y git
-
-  # Install ansible
-  DEBIAN_FRONTEND=noninteractive apt install -y ansible
+# Detect the operating system
+OS="$(uname)"
+if [ "$OS" == "Darwin" ] || [ "$OS" == "Linux" ]; then
+    install_brew
+else
+    echo "Unsupported operating system: $OS"
+    exit 1
 fi
 
-# Create the .config/ansible directory
-mkdir -p "$XDG_CONFIG_HOME/ansible"
 
-# Move the existing ansible.cfg file to the new location (if it exists)
-if [ -f "$HOME/.ansible.cfg" ]; then
-  mv "$HOME/.ansible.cfg" "$XDG_CONFIG_HOME/ansible/ansible.cfg"
-fi
 
-# Add remote_tmp and local_tmp settings to the ansible.cfg file
-echo '[defaults]' >> "$XDG_CONFIG_HOME/ansible/ansible.cfg"
-echo 'remote_tmp = $XDG_CONFIG_HOME/ansible/tmp' >> "$XDG_CONFIG_HOME/ansible/ansible.cfg"
-echo 'local_tmp = $XDG_CONFIG_HOME/ansible/tmp' >> "$XDG_CONFIG_HOME/ansible/ansible.cfg"
+# # Set XDG_CONFIG_HOME and ZDOTDIR variables in .zshenv
+# mkdir "~/.config"
+# echo 'export XDG_CONFIG_HOME="~"' >>~/.zshenv
+# echo 'export ZDOTDIR="$XDG_CONFIG_HOME"' >>~/.zshenv
 
-echo "Please wait. This may take several minutes..."
+# if [[ "$(uname)" == "Darwin" ]]; then
+#   # Check if Homebrew is installed
+#   if ! command -v brew &>/dev/null; then
+#     # Install Homebrew
+#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#   else
+#     # Update Homebrew
+#     brew update
+#   fi
 
-# Run the Ansible playbook
-ansible-pull -U "https://github.com/RATIU5/ansible-config.git"
+#   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+#   # Add Homebrew shell environment to .zshrc
+#   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>"~/.zshrc"
+#   eval "$(/opt/homebrew/bin/brew shellenv)"
+
+#   # Install ansible
+#   brew install ansible
+
+# elif [[ "$(uname)" == "Linux" ]]; then
+#   # Update package manager
+#   apt update
+
+#   # Install git
+#   apt install -y git
+
+#   # Install ansible
+#   DEBIAN_FRONTEND=noninteractive apt install -y ansible
+# fi
+
+# # Create the .config/ansible directory
+# mkdir -p "$XDG_CONFIG_HOME/ansible"
+
+# # Move the existing ansible.cfg file to the new location (if it exists)
+# if [ -f "$HOME/.ansible.cfg" ]; then
+#   mv "$HOME/.ansible.cfg" "$XDG_CONFIG_HOME/ansible/ansible.cfg"
+# fi
+
+# # Add remote_tmp and local_tmp settings to the ansible.cfg file
+# echo '[defaults]' >> "$XDG_CONFIG_HOME/ansible/ansible.cfg"
+# echo 'remote_tmp = $XDG_CONFIG_HOME/ansible/tmp' >> "$XDG_CONFIG_HOME/ansible/ansible.cfg"
+# echo 'local_tmp = $XDG_CONFIG_HOME/ansible/tmp' >> "$XDG_CONFIG_HOME/ansible/ansible.cfg"
+
+# echo "Please wait. This may take several minutes..."
+
+# # Run the Ansible playbook
+# ansible-pull -U "https://github.com/RATIU5/ansible-config.git"
