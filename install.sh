150e82a1c30aa8ad619b3a24ff37c4b04a6374f2
RATIU5 <jcmemmott20@outlook.com>
2023-05-24 19:58:20 -0600

    rewrite: pure install brew & ansible & install config

--- install.sh
+++ install.sh
@@ -1,153 +1,63 @@
 #!/bin/bash
 
-# Function to install zsh on macOS and common Linux distros
-install_zsh() {
-  if [ "$(uname)" == "Darwin" ]; then
-    # zsh is installed by default on macOS
-    # brew install zsh
-    echo "zsh installed on macOS..."
-  elif command -v apt &>/dev/null; then
-    sudo apt install zsh
-  elif command -v yum &>/dev/null; then
-    sudo yum install zsh
-  elif command -v pacman &>/dev/null; then
-    sudo pacman -S zsh
-  else
-    echo "Unsupported package manager. Install zsh manually."
-    exit 1
-  fi
-
-  if [ ! -f "$HOME/.zshenv" ]; then
-      touch "$HOME/.zshenv"
-  fi
-
-  if [ ! -d "$HOME/.config" ]; then
-    mkdir -p "$HOME/.config"
-  fi
-
-  echo 'export XDG_CONFIG_HOME="$HOME/.config"' >> "$HOME/.zshenv"
-  echo 'export ZDOTDIR="$XDG_CONFIG_HOME/zsh"' >> "$HOME/.zshenv"
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
 }
 
-# Function to install brew on macOS or Linux
+is_linux() {
+  [ "$(uname)" == "Linux" ] && return 0 || return 1
+}
+
+is_mac() {
+  [ "$(uname)" == "Darwin" ] && return 0 || return 1
+}
+
+file_exists() {
+  [ -f "$1" ] && return 0 || return 1
+}
+
+ensure_file_exists() {
+  if ! file_exists "$1"; then
+    touch "$1"
+  fi
+}
+
 install_brew() {
-    if ! command -v brew &>/dev/null; then
-        echo "Installing brew..."
-
-        # Install dependencies for Linux
-        if [ "$(uname)" == "Linux" ]; then
-            if command -v apt &>/dev/null; then
-                apt update
-                apt install -y build-essential procps curl file git
-            elif command -v yum &>/dev/null; then
-                yum -y groupinstall 'Development Tools'
-                yum -y install procps-ng curl file git
-            else
-                echo "Unsupported package manager. Install dependencies manually."
-                exit 1
-            fi
+    # Install dependencies for Linux
+    if is_linux; then
+        if command_exists apt; then
+            apt update
+            apt install -y build-essential git
+        elif command_exists yum; then
+            yum -y groupinstall 'Development Tools'
+            yum -y install git
+        else
+            echo "Unsupported package manager. Install dependencies manually."
+            exit 1
         fi
-
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-        # Append brew path
-        if [ "$(uname)" == "Darwin" ]; then
-            # Check for .zshrc and create if they don't exist
-            if [ ! -f "$ZDOTDIR/.zshrc" ]; then
-                touch "$ZDOTDIR/.zshrc"
-            fi
-            echo 'eval "$(/usr/local/bin/brew shellenv)"' >> $ZDOTDIR/.zshrc
-            source $ZDOTDIR/.zshrc
-        else  # Assuming Linux
-            # Check for .bashrc and create if they don't exist
-            if [ ! -f "$HOME/.bashrc" ]; then
-                touch "$HOME/.bashrc"
-            fi
-            echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
-            eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-            source ~/.bashrc
-            brew install zsh
-
-            if [ ! -f "$$ZDOTDIR/.zshrc" ]; then
-                touch "$$ZDOTDIR/.zshrc"
-            fi
-            echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $ZDOTDIR/.zshrc
-        fi
-    else
-        echo "Brew is already installed."
     fi
+
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
-# Detect the operating system
-OS="$(uname)"
-if [ "$OS" == "Darwin" ] || [ "$OS" == "Linux" ]; then
-    install_zsh
-    install_brew
-else
+if ! is_mac && ! is_linux; then
     echo "Unsupported operating system: $OS"
     exit 1
 fi
 
-# Install git on macOS
-if [ "$OS" == "Darwin" ]; then
-    brew install git
+if ! command_exists brew; then
+    install_brew
+    echo "Homebrew installed."
+else
+    echo "Homebrew already installed."
 fi
 
-# Install Ansible
-brew install ansible
+if ! command_exists ansible; then
+    brew install ansible
+    echo "Ansible installed."
+else 
+    echo "Ansible already installed."
+fi
 
 ansible-pull -U "https://github.com/RATIU5/ansible-config.git"
-
-
-
-# # Set XDG_CONFIG_HOME and ZDOTDIR variables in .zshenv
-# mkdir "~/.config"
-# echo 'export XDG_CONFIG_HOME="~"' >>~/.zshenv
-# echo 'export ZDOTDIR="$XDG_CONFIG_HOME"' >>~/.zshenv
-
-# if [[ "$(uname)" == "Darwin" ]]; then
-#   # Check if Homebrew is installed
-#   if ! command -v brew &>/dev/null; then
-#     # Install Homebrew
-#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-#   else
-#     # Update Homebrew
-#     brew update
-#   fi
-
-#   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-#   # Add Homebrew shell environment to .zshrc
-#   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>"~/.zshrc"
-#   eval "$(/opt/homebrew/bin/brew shellenv)"
-
-#   # Install ansible
-#   brew install ansible
-
-# elif [[ "$(uname)" == "Linux" ]]; then
-#   # Update package manager
-#   apt update
-
-#   # Install git
-#   apt install -y git
-
-#   # Install ansible
-#   DEBIAN_FRONTEND=noninteractive apt install -y ansible
-# fi
-
-# # Create the .config/ansible directory
-# mkdir -p "$XDG_CONFIG_HOME/ansible"
-
-# # Move the existing ansible.cfg file to the new location (if it exists)
-# if [ -f "$HOME/.ansible.cfg" ]; then
-#   mv "$HOME/.ansible.cfg" "$XDG_CONFIG_HOME/ansible/ansible.cfg"
-# fi
-
-# # Add remote_tmp and local_tmp settings to the ansible.cfg file
-# echo '[defaults]' >> "$XDG_CONFIG_HOME/ansible/ansible.cfg"
-# echo 'remote_tmp = $XDG_CONFIG_HOME/ansible/tmp' >> "$XDG_CONFIG_HOME/ansible/ansible.cfg"
-# echo 'local_tmp = $XDG_CONFIG_HOME/ansible/tmp' >> "$XDG_CONFIG_HOME/ansible/ansible.cfg"
-
-# echo "Please wait. This may take several minutes..."
-
-# # Run the Ansible playbook
-# ansible-pull -U "https://github.com/RATIU5/ansible-config.git"
